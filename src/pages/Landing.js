7cfce57e2dda5a5b9a0936aad9c8e8851e22b0b0
umut guven <ggvenumut@gmail.com>
2023-01-14 02:52:23 +0300

    Style Landing Page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,24 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
-
+import Wrapper from "../assets/wrappers/LandingPage";
 function Landing() {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>some text</p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 }
 export default Landing;
