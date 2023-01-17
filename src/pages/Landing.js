9351069af64083d60325e488fa34cf777cdd3f91
umut guven <ggvenumut@gmail.com>
2023-01-17 04:37:29 +0300

    Logo Component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,11 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
 function Landing() {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobster logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         {/* info */}
@@ -13,7 +13,11 @@ function Landing() {
           <h1>
             job <span>tracking</span> app
           </h1>
-          <p>some text</p>
+          <p>
+            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
+            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
+            crucifix heirloom meggings bicycle rights.
+          </p>
           <button className="btn btn-hero">Login/Register</button>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
