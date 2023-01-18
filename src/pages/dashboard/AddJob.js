04a79f7c327951c1d51edb68e74646227cb96df9
umut guven <ggvenumut@gmail.com>
2023-01-18 11:24:23 +0300

    Create Job Request

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -2,7 +2,11 @@ import { FormRow, FormRowSelect } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import { clearValues, handleChange } from "../../features/job/jobSlice";
+import {
+  clearValues,
+  handleChange,
+  createJob,
+} from "../../features/job/jobSlice";
 
 function AddJob() {
   const {
@@ -26,6 +30,7 @@ function AddJob() {
       toast.error("Please fill out all fields");
       return;
     }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
 
   const handleJobInput = (e) => {
