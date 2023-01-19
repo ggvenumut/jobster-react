2dc97f7a511974a992f635c18ff529932562a376
umut guven <ggvenumut@gmail.com>
2023-01-19 03:55:33 +0300

    GetAllJobs Request - Comlete

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -3,9 +3,15 @@ import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useSelector, useDispatch } from "react-redux";
 import Loading from "./Loading";
+import { getAllJobs } from "../features/allJobs/allJobsSlice";
+
 function JobsContainer() {
   const { jobs, isLoading } = useSelector((store) => store.allJobs);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
 
   if (isLoading) {
     return <Loading />;
@@ -18,6 +24,7 @@ function JobsContainer() {
       </Wrapper>
     );
   }
+
   return (
     <Wrapper>
       <h5>jobs info</h5>
